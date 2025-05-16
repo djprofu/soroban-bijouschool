972331accf35d1495a660a796cd60dfcece97a12
Cristian Olaru <cristian_olaru_30@yahoo.com>
2025-05-16 18:16:13 +0300

    Actualizări login și panou profesor + export utilizatori.js

---
+++ utilizatori.js
@@ -0,0 +1,14 @@
+const utilizatori = [
+  {
+    rol: "profesor",
+    nume: "Administrator",
+    username: "admin",
+    parola: "1986"
+  },
+  {
+    rol: "profesor",
+    nume: "Bucur Cristina",
+    username: "prof-sq2d",
+    parola: "ek75k4"
+  }
+];
